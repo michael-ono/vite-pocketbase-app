1429b056e1b052879253e9b88cce13308cf9a75e
michael-ono <mikeonochie@outlook.com>
2023-11-08 00:00:19 +0100

    added styled button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <h3>Create a task today</h3>
-      <button>Task</button>
+      <StyledButton>Task</StyledButton>
     </div>
   )
 }
+
+const StyledButton = styled.button`
+  background-color: #f0f0f0;
+  color: #333;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 25px;
+  cursor: pointer;
+`;
